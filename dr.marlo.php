f27ab338f10cfb836cf5937a45e1091347bcf3a2
James Wallace <sausages@gmail.com>
2024-06-17 11:36:21 -0400

    feat: initial commit

---
+++ dr.marlo.php
@@ -0,0 +1,25 @@
+#!/usr/bin/php
+<?php
+include 'include/config.inc';
+
+/*
+echo json_decode('"\u2300"');
+echo json_decode('"\u2316"');
+echo json_decode('"\u2318"');
+echo json_decode('"\u2327"');
+echo json_decode('"\u232C"');
+echo json_decode('"\u2332"');
+echo json_decode('"\u2337"');
+echo json_decode('"\u2338"');
+echo json_decode('"\u2339"');
+echo json_decode('"\u233A"');
+echo json_decode('"\u233B"');
+echo json_decode('"\u233D"');
+echo json_decode('"\u233E"');
+echo json_decode('"\u2341"');
+echo json_decode('"\u2349"');
+echo json_decode('"\u23F9"');
+echo json_decode('"\u23FA"');
+*/
+
+$g = new Game();
